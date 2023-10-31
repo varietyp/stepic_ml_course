c40ee7c82550df9cea41d6cb6ad4583cd8c7886a
varya_pp <varyamint@gmail.com>
2023-10-31 14:45:35 +0300

    not needed group by func deleted

--- data_analysis/data_plot_2.py
+++ data_analysis/data_plot_2.py
@@ -5,16 +5,6 @@ import seaborn as sns
 sns.set(rc={'figure.figsize': (9, 6)})
 
 events_data = pd.read_csv('event_data_train.csv')
-# events_data[events_data.action == 'passed'].groupby('user_id', as_index=False)\
-#    .aggregate({'step_id': 'count'})\
-#    .rename(columns={'step_id': 'passed_steps'}).head()
-# events_data[events_data.action == 'passed'].groupby('user_id', as_index=False)\
-#    .aggregate({'step_id': 'count'})\
-#    .rename(columns={'step_id': 'passed_steps'}).passed_steps.hist()
-
-# events_data[events_data.action == 'passed'].groupby('user_id', as_index=False)\
-#    .aggregate({'step_id': 'count'})\
-#    .rename(columns={'step_id': 'passed_steps'}).passed_steps.min()
 
 actions_data = events_data.pivot_table(index='user_id',
                                        columns='action', values='step_id',
